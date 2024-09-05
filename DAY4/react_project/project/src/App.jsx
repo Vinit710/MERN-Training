6b8e2cb7d34bd6aa63f2e27d3eb2ce91fcba0b9c
Vinit710 <vinitdesai4933@gmail.com>
2024-09-05 16:18:47 +0530

    day 4 afternoon

---
+++ DAY4/react_project/project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+
+
+
+export default function App() {
+
+  return(
+    <div>
+      <h1 id='idd'>Black Myth Wukong</h1>
+      <Navbar />
+      <Navbar />
+      <Navbar />
+    </div>
+  )
+  
+}
---
+++ DAY4/react_project/project/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import myImage from "../assets/image.jpg"; // Go up one level if it's in src/assets
+
+
+
+export default function() {
+  return (
+    <div>
+        <h1>componet</h1>
+        <img src={myImage} alt="My Image" />
+
+        <button onClick={handlezzzzzzzzzzzzzzzzzzzzzzzzzzzz}></button>
+
+
+        
+
+    </div>
+  )
+}
